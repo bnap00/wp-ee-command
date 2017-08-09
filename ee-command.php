f34d8c126894fc7ce8e9af1bf14597c19cce3dd5
Bharat Parsiya <bnap00@gmail.com>
2017-08-09 11:49:53 +0530

    add update command

--- ee-command.php
+++ ee-command.php
@@ -69,8 +69,30 @@ class EE_DB extends SQLite3 {
 	 */
 	public function insert_site( $site_name, $site_type, $cache_type, $php_version, $sql_username, $sql_db_name, $sql_password, $multisite ) {
 		$query = 'INSERT INTO site_data (site_name, site_type, cache_type, php_version, sql_username, sql_db_name, sql_password, multi_site) VALUES ( \'' . $site_name . '\', \'' . $site_type . '\', \'' . $cache_type . '\', \'' . $php_version . '\', \'' . $sql_username . '\', \'' . $sql_db_name . '\', \'' . $sql_password . '\', \'' . $multisite . '\');';
-		WP_CLI ::warning( $query );
 		if ( $this -> query( $query ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Update site to new specifications.
+	 *
+	 * @param string $site_name    Name of the site.
+	 * @param string $site_type    site type. One of 'wp', 'php', 'html'.
+	 * @param string $cache_type   cache type of the site. One of 'total_cache', 'super_cache', 'fast_cgi_cache', 'redis_cache').
+	 * @param string $php_version  php version of the site. One of '5.6', '7', 'disabled'.
+	 * @param string $sql_username sql username for the site.
+	 * @param string $sql_db_name  sql database name.
+	 * @param string $sql_password sql password of the site.
+	 * @param string $multisite    type of multi site One of the 'subdirectory', 'subdomain', 'disabled'.
+	 *
+	 * @return bool if the
+	 */
+	public function update_site( $site_name, $site_type, $cache_type, $php_version, $sql_username, $sql_db_name, $sql_password, $multisite ) {
+		$query = 'UPDATE site_data SET `site_type` = \'' . '\' `cache_type` = \'' . $cache_type . '\' `php_version` = \'' . $php_version . '\' `sql_username` = \'' . $sql_username . '\' `sql_db_name` = \'' . $sql_db_name . '\' `sql_password` = \'' . $sql_password . '\' `multi_site` = \'' . $multisite . '\' WHERE `site_name` = \'' . $site_name . '\'';
+		if ( $this -> exec( $query ) ) {
 			return true;
 		} else {
 			return false;
@@ -146,7 +168,7 @@ if ( ! class_exists( 'EE_Site_Command' ) && class_exists( 'EE_DB' ) ) {
 	class EE_Site_Command extends WP_CLI_Command {
 
 		/**
-		 * Create site.
+		 * Create new site.
 		 *
 		 * ## OPTIONS
 		 *
@@ -195,11 +217,11 @@ if ( ! class_exists( 'EE_Site_Command' ) && class_exists( 'EE_DB' ) ) {
 			$db = new EE_DB();
 			$db -> init();
 
+			if ( ! isset( $args[ 0 ] ) || empty( $args[ 0 ] ) ) {
+				WP_CLI ::error( 'You cannot update site without sitename' );
+			}
 			if ( $db -> site_exists( $args[ 0 ] ) ) {
 				WP_CLI ::error( 'Site Already existing with domain : ' . $args[ 0 ] );
-			}
-			if ( ! isset( $args[ 0 ] ) || empty( $args[ 0 ] ) ) {
-				WP_CLI ::error( 'You cannot create site without sitename' );
 			}
 			if ( isset( $ass_args[ 'wpsubdir' ] ) && $ass_args[ 'wpsubdir' ] ) {
 				if ( isset( $ass_args[ 'wpsubdom' ] ) && $ass_args[ 'wpsubdom' ] ) {
@@ -328,6 +350,193 @@ if ( ! class_exists( 'EE_Site_Command' ) && class_exists( 'EE_DB' ) ) {
 		}
 
 		/**
+		 * Create site.
+		 *
+		 * ## OPTIONS
+		 *
+		 * <site_name>
+		 * :Site name to be created.
+		 *
+		 * [--html]
+		 * : Update to html site.
+		 *
+		 * [--php]
+		 * : Update site to php 5.6.
+		 *
+		 * [--php7]
+		 * : Update site to php7.
+		 *
+		 * [--mysql]
+		 * : Update site to php and mysql database.
+		 *
+		 * [--wp]
+		 * : Update site to wordpress installed.
+		 *
+		 * [--wpfc]
+		 * : Update site to wordpress + nginx fastcgi_cache.
+		 *
+		 * [--w3tc]
+		 * : Update site to wordpress with w3-total-cache plugin.
+		 *
+		 * [--wpsc]
+		 * : Update site to wordpress with whisp-super-cache plugin.
+		 *
+		 * [--wpredis]
+		 * : Update site to wordpress + nginx redis_cache.
+		 *
+		 * [--wpsubdir]
+		 * : Update wordpress site to multi site with subdirectory.
+		 *
+		 * [--wpsubdom]
+		 * : Update wordpress site to multi site with subdomain.
+		 *
+		 * @when before_wp_load
+		 *
+		 * @param array $args     arguments for the command.
+		 * @param array $ass_args associative arguments for the command.
+		 */
+		public function update( $args, $ass_args ) {
+			$db = new EE_DB();
+			$db -> init();
+
+			if ( ! isset( $args[ 0 ] ) || empty( $args[ 0 ] ) ) {
+				WP_CLI ::error( 'You cannot update site without sitename' );
+			}
+			if ( ! $db -> site_exists( $args[ 0 ] ) ) {
+				WP_CLI ::error( 'Site does not exist with domain : ' . $args[ 0 ] );
+			}
+			if ( isset( $ass_args[ 'wpsubdir' ] ) && $ass_args[ 'wpsubdir' ] ) {
+				if ( isset( $ass_args[ 'wpsubdom' ] ) && $ass_args[ 'wpsubdom' ] ) {
+					WP_CLI ::error( 'you cannot create wp subdir site with wp subdomain site' );
+				}
+				if ( isset( $ass_args[ 'html' ] ) && $ass_args[ 'html' ] ) {
+					WP_CLI ::error( 'you cannot create a wordpress site with html' );
+				}
+				$multisite = 'subdirectory';
+				$site_type = 'wp';
+			}
+			if ( isset( $ass_args[ 'wpsubdom' ] ) && $ass_args[ 'wpsubdom' ] ) {
+				if ( isset( $ass_args[ 'wpsubdir' ] ) && $ass_args[ 'wpsubdir' ] ) {
+					WP_CLI ::error( 'you cannot create wp subdir site with wp subdomain site' );
+				}
+				if ( isset( $ass_args[ 'html' ] ) && $ass_args[ 'html' ] ) {
+					WP_CLI ::error( 'you cannot create a wordpress site with html' );
+				}
+				$multisite = 'subdomain';
+				$site_type = 'wp';
+			}
+
+			if ( isset( $ass_args[ 'w3tc' ] ) && $ass_args[ 'w3tc' ] ) {
+				if ( isset( $ass_args[ 'html' ] ) && $ass_args[ 'html' ] ) {
+					WP_CLI ::error( 'you cannot create a wordpress site with html' );
+				}
+				if ( isset( $ass_args[ 'wpsc' ] ) && $ass_args[ 'wpsc' ] ) {
+					WP_CLI ::error( 'cannot combine w3tc with wpsc' );
+				}
+				if ( isset( $ass_args[ 'wpfc' ] ) && $ass_args[ 'wpfc' ] ) {
+					WP_CLI ::error( 'cannot combine w3tc with wpfc' );
+				}
+				if ( isset( $ass_args[ 'wpredis' ] ) && $ass_args[ 'wpredis' ] ) {
+					WP_CLI ::error( 'cannot combine w3tc with wpredis' );
+				}
+				$cache_type = 'total_cache';
+				$site_type  = 'wp';
+			}
+			if ( isset( $ass_args[ 'wpsc' ] ) && $ass_args[ 'wpsc' ] ) {
+				if ( isset( $ass_args[ 'html' ] ) && $ass_args[ 'html' ] ) {
+					WP_CLI ::error( 'you cannot create a wordpress site with html' );
+				}
+				if ( isset( $ass_args[ 'wpfc' ] ) && $ass_args[ 'wpfc' ] ) {
+					WP_CLI ::error( 'cannot combine wpsc with wpfc' );
+				}
+				if ( isset( $ass_args[ 'wpredis' ] ) && $ass_args[ 'wpredis' ] ) {
+					WP_CLI ::error( 'cannot combine wpsc with wpredis' );
+				}
+				$cache_type = 'super_cache';
+				$site_type  = 'wp';
+			}
+			if ( isset( $ass_args[ 'wpfc' ] ) && $ass_args[ 'wpfc' ] ) {
+				if ( isset( $ass_args[ 'html' ] ) && $ass_args[ 'html' ] ) {
+					WP_CLI ::error( 'you cannot create a wordpress site with html' );
+				}
+				if ( isset( $ass_args[ 'wpredis' ] ) && $ass_args[ 'wpredis' ] ) {
+					WP_CLI ::error( 'cannot combine wpfc with wpfc' );
+				}
+				$cache_type = 'fast_cgi_cache';
+				$site_type  = 'wp';
+			}
+			if ( isset( $ass_args[ 'wpredis' ] ) && $ass_args[ 'wpredis' ] ) {
+				if ( isset( $ass_args[ 'html' ] ) && $ass_args[ 'html' ] ) {
+					WP_CLI ::error( 'you cannot create a wordpress site with html' );
+				}
+				$cache_type = 'redis_cache';
+				$site_type  = 'wp';
+			}
+
+			if ( ( isset( $ass_args[ 'wp' ] ) && $ass_args[ 'wp' ] ) ) {
+				if ( isset( $ass_args[ 'html' ] ) && $ass_args[ 'html' ] ) {
+					WP_CLI ::error( 'you cannot create a php site with html' );
+				}
+				$site_type = 'wp';
+			}
+
+			if ( ( isset( $ass_args[ 'php' ] ) && $ass_args[ 'php' ] ) || ( isset( $ass_args[ 'php7' ] ) && $ass_args[ 'php7' ] ) ) {
+				if ( isset( $ass_args[ 'html' ] ) && $ass_args[ 'html' ] ) {
+					WP_CLI ::error( 'you cannot create a php site with html' );
+				}
+				$php_version = 5.6;
+				if ( isset( $ass_args[ 'php7' ] ) && $ass_args[ 'php7' ] ) {
+					$php_version = 7;
+				}
+				$site_type = isset( $site_type ) && 'wp' === $site_type ? 'wp' : 'php';
+			}
+			if ( ( isset( $ass_args[ 'mysql' ] ) && $ass_args[ 'mysql' ] ) ) {
+				if ( isset( $ass_args[ 'html' ] ) && $ass_args[ 'html' ] ) {
+					WP_CLI ::error( 'you cannot create a php site with html' );
+				}
+				$mysql = true;
+			}
+
+			if ( isset( $ass_args[ 'html' ] ) && $ass_args[ 'html' ] ) {
+				$site_type = 'html';
+			}
+			if ( isset( $site_type ) && 'wp' === $site_type ) {
+				$mysql = true;
+			}
+
+
+			$db = new EE_DB();
+			$db-> init();
+			$current_settings = $db->site_info($args[0]);
+
+			if ( 'disabled' !== $current_settings['multi_site'] && $current_settings['multi_site'] !== $multisite ) {
+				WP_CLI::error( 'You cannot change type of multi site.' );
+			}
+
+			if ( isset( $mysql ) && $mysql && empty( $current_settings['sql_username'] ) ) {
+				// Create mysqlcreds.
+				$sql_username = str_replace( '.', '_', $args[ 0 ] );
+				$sql_db_name  = $sql_username;
+				$sql_password = $this -> _randomPassword();
+
+			}
+			$site_type    = isset( $site_type )    ? $site_type    : $current_settings['site_type'];
+			$cache_type   = isset( $cache_type )   ? $cache_type   : $current_settings['cache_type'];
+			$php_version  = isset( $php_version )  ? $php_version  : $current_settings['php_version'];
+			$multisite    = isset( $multisite )    ? $multisite    : $current_settings['multi_site'];
+			$sql_username = isset( $sql_username ) ? $sql_username : $current_settings['sql_username'];
+			$sql_db_name  = isset( $sql_db_name )  ? $sql_db_name  : $current_settings['sql_db_name'];
+			$sql_password = isset( $sql_password ) ? $sql_password : $current_settings['sql_password'];
+
+
+			if ( $db -> update_site( $args[ 0 ], $site_type, $cache_type, $php_version, $sql_username, $sql_db_name, $sql_password, $multisite ) ) {
+				WP_CLI::success( 'Site created successfully' );
+			} else {
+				WP_CLI::error( 'An error occured' );
+			}
+		}
+
+		/**
 		 * List all the site.
 		 *
 		 * @when before_wp_load
@@ -411,13 +620,15 @@ if ( ! class_exists( 'EE_Site_Command' ) && class_exists( 'EE_DB' ) ) {
 		private function _show_in_table( $data ) {
 			$result = array();
 			foreach ( $data as $key => $value ) {
-				array_push(
-					$result,
-					array(
-						'key'   => $key,
-						'value' => $value,
-					)
-				);
+				if ( ! empty( $value ) && 'disabled' !== $value ) {
+					array_push(
+						$result,
+						array(
+							'key'   => $key,
+							'value' => $value,
+						)
+					);
+				}
 			}
 			WP_CLI\Utils\format_items(
 				'table',
